6a40c639bcdeb5dd16c12d069d7d64a2a4d63061
Ted Business <hu1314v5@outlook.com>
2016-04-18 19:49:34 +0800

    发现，给朋友圈加个点击，可以与上一节一起

--- src/com/wuxinaedu/weixin/activity/fragment/FindFragment.java
+++ src/com/wuxinaedu/weixin/activity/fragment/FindFragment.java
@@ -1,20 +1,36 @@
 package com.wuxinaedu.weixin.activity.fragment;
 
+import com.wuxinaedu.weixin.R;
+import com.wuxinaedu.weixin.utils.L;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 
-public class FindFragment extends Fragment{
-	
+public class FindFragment extends Fragment {
+
 	private View view;
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		if(view != null){
+		if (view != null) {
 			return view;
 		}
-		return view ;
+		view = inflater.inflate(R.layout.fragment_find, null);
+		view.findViewById(R.id.friends_id).setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+//				Intent intent = new Intent(getActivity(), FriendsActivity.class);
+//				UserInfor userBasicInfor = ((MainActivity) getActivity()).getUserInfo();
+//				intent.putExtra(Constant.USER_INFOR, userBasicInfor);
+//				startActivity(intent);
+				L.i("朋友圈");
+			}
+		});
+		return view;
 	}
 }
