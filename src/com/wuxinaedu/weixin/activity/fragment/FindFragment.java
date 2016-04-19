0dec647b604b3d264813a3e9c1706dea4c4973ca
Ted Business <hu1314v5@outlook.com>
2016-04-19 10:05:10 +0800

    朋友圈显示，差最后一个点击图片

--- src/com/wuxinaedu/weixin/activity/fragment/FindFragment.java
+++ src/com/wuxinaedu/weixin/activity/fragment/FindFragment.java
@@ -1,8 +1,13 @@
 package com.wuxinaedu.weixin.activity.fragment;
 
 import com.wuxinaedu.weixin.R;
+import com.wuxinaedu.weixin.activity.FriendsActivity;
+import com.wuxinaedu.weixin.activity.MainActivity;
+import com.wuxinaedu.weixin.bean.UserInfor;
+import com.wuxinaedu.weixin.utils.Constant;
 import com.wuxinaedu.weixin.utils.L;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -24,10 +29,10 @@ public class FindFragment extends Fragment {
 
 			@Override
 			public void onClick(View v) {
-//				Intent intent = new Intent(getActivity(), FriendsActivity.class);
-//				UserInfor userBasicInfor = ((MainActivity) getActivity()).getUserInfo();
-//				intent.putExtra(Constant.USER_INFOR, userBasicInfor);
-//				startActivity(intent);
+				Intent intent = new Intent(getActivity(), FriendsActivity.class);
+				UserInfor userBasicInfor = ((MainActivity) getActivity()).getUserInfo();
+				intent.putExtra(Constant.USER_INFOR, userBasicInfor);
+				startActivity(intent);
 				L.i("朋友圈");
 			}
 		});
